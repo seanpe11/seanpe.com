2fa36172a443062f768f67ab87fe648a5eb2876c
seanpe11 <sean.m.s.pe@gmail.com>
2023-05-15 16:05:47 -0500

    added working keylogging

--- src/pages/race.tsx
+++ src/pages/race.tsx
@@ -1,25 +1,32 @@
 import { type NextPage } from "next";
 import { useState, useEffect } from "react";
+import { ALLOWED_KEYS } from "~/utils/constants";
 import Head from "next/head";
 import Link from "next/link";
 
 
 const Race: NextPage = () => {
-  const [typed, setTyped] = useState("")
+  const [typed, setTyped] = useState<Array<KeyboardEvent>>([])
+  const [outString, setOutString] = useState('')
 
-  const handleKeyPress = (event) => {
-    console.log(event.key + " " + new Date())
-    const {timestamp, key, code} = event
-    setTyped(typed + key)
+  const onKeyPress = (event: KeyboardEvent) => {
+    setTyped(prev => [...prev, event])
+    if (ALLOWED_KEYS.includes(event.key))
+      setOutString(prev => prev + event.key)
   }
+
+
   useEffect(() => {
-      window.addEventListener("keydown", handleKeyPress)
+      window.addEventListener("keydown", onKeyPress)
   }, []);
 
   return(
     <>
       <div >
-        Race!!!
+        {typed.map(k => <>{k.key}</>)}
+      </div>
+      <div>
+        {outString}
       </div>
     </>
   )
