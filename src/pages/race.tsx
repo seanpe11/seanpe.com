3fbf2b4a268fa8d84591deac070e24fb372d6c13
seanpe11 <sean.m.s.pe@gmail.com>
2023-05-15 16:29:04 -0500

    TODO: track progress of text

--- src/pages/race.tsx
+++ src/pages/race.tsx
@@ -4,15 +4,26 @@ import { ALLOWED_KEYS } from "~/utils/constants";
 import Head from "next/head";
 import Link from "next/link";
 
+const sampleText = "It's a praying mantis. Do you know how they mate? The male will sneak up on the female and she'll bite off his head and the rest of his body will keep on mating and when they're done she'll eat him. She'll eat the rest of him."
 
 const Race: NextPage = () => {
   const [typed, setTyped] = useState<Array<KeyboardEvent>>([])
   const [outString, setOutString] = useState('')
+  const [prompt, setPrompt] = useState(sampleText)
+  // used to track where user is
+  const [loc, setLoc] = useState(0)
 
   const onKeyPress = (event: KeyboardEvent) => {
     setTyped(prev => [...prev, event])
-    if (ALLOWED_KEYS.includes(event.key))
+
+    if (event.key == 'Backspace') {
+        setOutString(prev => prev.slice(0, -1))
+    }
+
+    if (ALLOWED_KEYS.includes(event.key)) {
+      setLoc(loc + 1)
       setOutString(prev => prev + event.key)
+    }
   }
 
 
@@ -22,11 +33,14 @@ const Race: NextPage = () => {
 
   return(
     <>
-      <div >
-        {typed.map(k => <>{k.key}</>)}
+      <div>
+        <span className="text-gray">{sampleText.substring(0,loc)}</span>{sampleText.substring(loc)}
       </div>
       <div>
         {outString}
+      </div>
+      <div >
+        {typed.map(k => <>{k.key}</>)}
       </div>
     </>
   )
