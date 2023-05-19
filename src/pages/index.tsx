371e229693888960bc481021a523df326b8888fd
seanpe11 <sean.m.s.pe@gmail.com>
2023-05-18 19:31:32 -0500

    created landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,18 +17,36 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#525252] to-[#040404]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Sean Pe
-          </h1>
+          <div>
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              Sean Pe
+            </h1>
+            {/* <h3 className="font-extrabold tracking-tight text-white sm:text-[1.8rem]"> */}
+            {/*   Fullstack Web Developer */}
+            {/* </h3> */}
+            <div className="flex justify-center items-center gap-4 my-2">
+              <Link href="https://github.com/seanpe11">
+                <svg className="fill-slate-400 hover:fill-white h-10 w-10" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>GitHub</title>
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+                </svg>
+              </Link>
+              <Link href="https://www.linkedin.com/in/sean-pe-84b1401a4/">
+                <svg className="fill-slate-400 hover:fill-white h-10 w-10" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <title>LinkedIn</title>
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                </svg>
+              </Link>
+            </div> </div>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="https://docs.google.com/document/d/1kS4Kngv_D16vuzcgklJRGzdMboTJEfwbrKpxoUHVgUc/export?format=pdf"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">Resume</h3>
               <div className="text-lg">
-
+                Click to download
               </div>
             </Link>
             <Link
@@ -44,6 +62,9 @@ const Home: NextPage = () => {
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            </p>
+            <p className="text-lg text-white">
+              - Denzel Washington
             </p>
           </div>
         </div>
