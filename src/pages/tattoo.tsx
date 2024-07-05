7105071b8dc2a9ce2451ec5d01e693c2401729b0
Sean Pe <sean.m.s.pe@gmail.com>
2024-07-05 20:26:48 +0800

    stupid apostrophe fix

--- src/pages/tattoo.tsx
+++ src/pages/tattoo.tsx
@@ -15,15 +15,15 @@ const Tattoo: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#525252] to-[#040404] text-white">
         <h1 className="text-4xl">I love you Mama!</h1>
         <p>This QR code is highly configurable, and thus can contain anything I want as a link.</p>
-        <h2>Here's a picture of you and me because I love you, to showcase that I really can put whatever I want on this website</h2>
+        <h2>Here&apos;s a picture of you and me because I love you, to showcase that I really can put whatever I want on this website</h2>
         <div>
           <h1>Arguments as to why I can have a tattoo</h1>
           <ul>
-            <li>I'm a big boy now</li>
+            <li>I&apos;m a big boy now</li>
             <li>I chose designs that are cool</li>
             <li>I chose placements that are concealable</li>
             <li>If anything, for the career path that I want, the tattoos I have would make me MORE hireable</li>
-            <li>I can explain each tattoo well, and the current website you're viewing is the other half of the QR Code tattoo</li>
+            <li>I can explain each tattoo well, and the current website you&apos;re viewing is the other half of the QR Code tattoo</li>
           </ul>
         </div>
       </main>
