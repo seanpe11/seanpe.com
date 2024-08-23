d1387619b799f8a21a96f193cdd43867dc621e0e
Sean Pe <sean.m.s.pe@gmail.com>
2024-08-23 13:09:16 -0500

    fix tattoo redirect

--- next.config.mjs
+++ next.config.mjs
@@ -12,22 +12,12 @@ const nextConfig = {
     return [
       {
         source: '/tattoo',
-        destination: '/home',
+        destination: '/',
         permanent: true,
       },
     ];
   },
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 
 export default nextConfig;
--- src/pages/tattoo.tsx
+++ src/pages/tattoo.tsx
@@ -1,7 +1,16 @@
-export default function Tattoo() {
-  return (
-    <div>
-      <h1>Tatoo</h1>
-    </div>
-  )
+import { GetServerSideProps } from 'next';
+
+// @ts-expect-error
+export const getServerSideProps: GetServerSideProps = () => {
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false, // Set to true if you want a permanent redirect (HTTP 301)
+    },
+  };
+};
+
+export default function TattooPage() {
+  return null; // This component will never be rendered because of the redirect
 }
+
